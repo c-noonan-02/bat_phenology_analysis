6af614f5a9ba4686ba6f39cf46456114d53415ab
c-noonan-02 <charlottenoonan@outlook.com>
2025-04-07 17:19:01 +0100

    Ran first practise analysis on activity data. Yet to finalise model and add moderators.

--- bat_phenology_analysis.R
+++ bat_phenology_analysis.R
@@ -41,12 +41,12 @@ metaanalysis_data$difference_variance <- metaanalysis_data$difference_SE^2
 ##### Meta-Analysis #####
 
 # build meta-analysis model of mean difference
-meta_model <- rma(yi = difference_means, sei = difference_SE, data = metaanalysis_data)
-meta_model
+timing_meta_model <- rma(yi = difference_means, sei = difference_SE, data = metaanalysis_data)
+timing_meta_model
 
 # plot the model - funnel plot
-funnel(meta_model)
-funnel(meta_model, label = "all", legend = list(cex = 0.9), back = "white", shade = "grey80", hlines = "grey90", lty = 2, lwd = 2, pch = 16, col = "#FF3399")
+funnel(timing_meta_model)
+funnel(timing_meta_model, label = "all", legend = list(cex = 0.9), back = "white", shade = "grey80", hlines = "grey90", lty = 2, lwd = 2, pch = 16, col = "#FF3399")
 
 # add labels explaining which points are associated with which papers
 study_labels <- c("4 = Zou et al. (2024)", "3 = Stone et al. (2009)", "2 = Mariton et al. (2022)", "1 = Lou et al. (2021)", "Studies:")
@@ -61,13 +61,85 @@ for (i in seq_along(study_labels)) {
   }}
 
 # plot the model - forest plot
-forest(meta_model, cex.lab = 0.8, cex.axis = 0.8, addfit = TRUE, shade = "zebra", order = "obs", col = "pink", border = "black", colout = "#FF3399")
+forest(timing_meta_model, cex.lab = 0.8, cex.axis = 0.8, addfit = TRUE, shade = "zebra", order = "obs", col = "pink", border = "black", colout = "#FF3399")
 
 
 #### 2. Meta-analysis on mean activity levels ####
+# clear environment
+rm(list = ls())
+
+# import relevant packages
+library(metafor)
 
 ##### Set-up #####
 # import data sets
+bat_passes_data <- read.csv("./activity_data/bat_passes_data.csv", sep = ",", header = TRUE)
+head(bat_passes_data)
+bibliography_data <- read.csv("./activity_data/activity_bibliography.csv", sep = ",", header = TRUE)
+head(bibliography_data)
+
+# # place papers in alphabetical order, as this is more logical
+# bibliography_data <- bibliography_data[order(bibliography_data$author), ]
+# bat_passes_data <- bat_passes_data[match(bibliography_data$paper_ID, bat_passes_data$paper_ID), ]
+
+
+##### Calculate Effect Sizes #####
+
+# calculate the difference between the means
+bat_passes_data$difference_means <- bat_passes_data$light_treatment_mean - bat_passes_data$dark_treatment_mean
+head(bat_passes_data)
+
+# calculate the associated SE
+bat_passes_data$difference_SE <- sqrt((bat_passes_data$light_se)^2 + (bat_passes_data$dark_se)^2)
+head(bat_passes_data)
+
+# visualise the data
+plot(bat_passes_data$difference_means, (1 / bat_passes_data$difference_SE), xlab = "Mean difference in mean activity (mean number of passess)", ylab = "Precision (1/SE)")
+
+# calculate variance
+bat_passes_data$difference_variance <- bat_passes_data$difference_SE^2
+
+
+##### Meta-Analysis #####
+
+# build meta-analysis model of mean difference
+activity_meta_model <- rma.mv(yi = difference_means, sei = difference_SE, data = bat_passes_data)
+activity_meta_model
+
+# trying to build random effect model
+activity_meta_model2 <- rma.mv(yi = difference_means, V = difference_variance, random = ~1|species, data = bat_passes_data)
+activity_meta_model2
+
+activity_meta_model3 <- rma.mv(yi = difference_means, V = difference_variance, mods = ~latitude, random = ~1|species, data = bat_passes_data)
+activity_meta_model3
+
+# plot the model - funnel plot
+funnel(activity_meta_model)
+funnel(activity_meta_model, label = "all", legend = list(cex = 0.9), back = "white", shade = "grey80", hlines = "grey90", lty = 2, lwd = 2, pch = 16, col = "#FF3399")
+
+plot(activity_meta_model$yi, activity_meta_model$sei, 
+     xlab = "Effect Size", 
+     ylab = "Standard Error (from model)", 
+     main = "Funnel Plot (Model-Based SE)",
+     pch = 16, col = "#FF3399")
+
+# # add labels explaining which points are associated with which papers
+# study_labels <- c("4 = Zou et al. (2024)", "3 = Stone et al. (2009)", "2 = Mariton et al. (2022)", "1 = Lou et al. (2021)", "Studies:")
+# xpos <- 38
+# ypos <- 13
+# rect(xpos, ypos - 7.4, xpos +34.4, ypos, col = "white", border = "black")
+# for (i in seq_along(study_labels)) {
+#   if (study_labels[i] == "Studies:") {
+#     text(xpos + 1.5, ypos - (i * 1.28), labels = study_labels[i], adj = 0, cex = 0.9, font = 2)
+#   } else {
+#     text(xpos + 1.5, ypos - (i * 1.28), labels = study_labels[i], adj = 0, cex = 0.9)
+#   }}
+
+# plot the model - forest plot
+forest(activity_meta_model, cex.lab = 0.8, cex.axis = 0.8, addfit = TRUE, shade = "zebra", order = "obs", col = "pink", border = "black", colout = "#FF3399")
+
+# change label from study to estimate, colour by species, etc.
+# add moderators
 
 
 #### 3. Assessment of Literature ####
